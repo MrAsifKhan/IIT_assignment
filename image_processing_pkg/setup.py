27fe22ee2c85a062e3cae60cc94d78c84566d14d
Asif Khan Pattan <100809935+MrAsifKhan@users.noreply.github.com>
2023-12-10 22:14:29 +0100

    updated setup.py
     added datafiles and node1 and nod2 entry points.

--- image_processing_pkg/setup.py
+++ image_processing_pkg/setup.py
@@ -7,19 +7,21 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/image_processing.launch.py'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='asif',
-    maintainer_email='asif@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='pattanasifkhan97@gmail.com',
+    description='IIT project ROS task',
+    license='AK',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'node1 = image_processing_pkg.node1:main',
+            'node2 = image_processing_pkg.node2:main',
         ],
     },
 )
